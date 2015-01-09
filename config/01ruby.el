2ba702bd3ef7c378b0dce4d178a6b5ca0002be07
Gabriel Arrillaga <gearril@sandia.gov>
2015-01-09 13:23:20 -0700

    Hook robe-mode into haml-mode.

--- config/01ruby.el
+++ config/01ruby.el
@@ -15,5 +15,6 @@
 
 ;;(add-hook 'ruby-mode-hook 'robe-mode)
 (add-hook 'enh-ruby-mode-hook 'robe-mode)
+(add-hook 'haml-mode-hook 'robe-mode)
 (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
   (rvm-activate-corresponding-ruby))
