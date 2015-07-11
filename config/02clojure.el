eb228b7092508c05bd486e13950445fc644a48f5
Gabriel Arrillaga <gearril@sandia.gov>
2015-07-11 10:56:21 -0600

    Clojure config.

--- config/02clojure.el
+++ config/02clojure.el
@@ -1,3 +1,31 @@
+;; A little more syntax highlighting
+;;(require 'clojure-mode-extra-font-locking)
+
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (setq nrepl-log-messages t)
 ;; (setq nrepl-hide-special-buffers t)
+
+;; Enable paredit for Clojure
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
+
+;; go right to the REPL buffer when it's finished connecting
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+;; When there's a cider error, show its buffer and switch to it
+(setq cider-show-error-buffer t)
+(setq cider-auto-select-error-buffer t)
+
+;; Where to store the cider history.
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+
+;; Wrap when navigating history.
+(setq cider-repl-wrap-history t)
+
+;; enable paredit in your REPL
+(add-hook 'cider-repl-mode-hook 'paredit-mode)
+
+;; Use clojure mode for other extensions
+(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.cljs.*$" . clojure-mode))
+;;(add-to-list 'auto-mode-alist '("lein-env" . enh-ruby-mode))
