a8a30d64cae56d681e86376ed6171a6c6e718aaf
Gabriel Arrillaga <gearril@sandia.gov>
2015-07-11 10:56:32 -0600

    More web-mode config.

--- config/04web.el
+++ config/04web.el
@@ -8,3 +8,9 @@
       (let ((web-mode-enable-part-face nil))
         ad-do-it)
     ad-do-it))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq web-mode-indent-style 2)
+(setq tab-width 2 indent-tabs-mode nil)
