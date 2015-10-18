08250ab65af6dec088f98b169c3a38527ea0d5e6
lustrousgorilla <lustrousgorilla@whisper.neomailbox.ch>
2015-10-18 10:55:20 -0600

    Navigate ido matches with C-n and C-p

--- config/11flx-ido.el
+++ config/11flx-ido.el
@@ -4,6 +4,9 @@
 (ido-vertical-mode 1)
 (ido-everywhere 1)
 (flx-ido-mode 1)
+;; shows count of matches
+;;(setq ido-vertical-show-count t)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
 ;; disable ido faces to see flx highlights
 (setq ido-enable-flex-matching)
 (setq ido-use-faces nil)
